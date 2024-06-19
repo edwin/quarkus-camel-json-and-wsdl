0a0dc9249fca18afd39a6f7d5d12ecab3550e8e1
edwin <edwinkun@gmail.com>
2024-06-19 14:35:43 +0700

    adding soap endpoint :snail: :mushroom:

---
+++ src/main/java/com/edw/service/EmployeeService.java
@@ -0,0 +1,44 @@
+package com.edw.service;
+
+import com.edw.soap.EmployeeServicePortType;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+import org.apache.camel.component.cxf.jaxws.CxfEndpoint;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+/**
+ * <pre>
+ *  com.edw.service.EmployeeService
+ * </pre>
+ *
+ * @author Muhammad Edwin < edwin at redhat dot com >
+ * 16 Jun 2024 22:41
+ */
+@ApplicationScoped
+public class EmployeeService {
+
+    @ConfigProperty(name="employee.soap.url")
+    private String employeeSoapUrl;
+
+    @Named
+    public CxfEndpoint employeeServiceEndpoint_getEmployeeById() {
+        CxfEndpoint cxfEndpoint = new CxfEndpoint();
+        cxfEndpoint.setServiceClass(EmployeeServicePortType.class);
+        cxfEndpoint.setAddress(employeeSoapUrl);
+        cxfEndpoint.setWsdlURL("/wsdl/employee.wsdl");
+        cxfEndpoint.setDefaultOperationName("GetEmployeeById");
+        cxfEndpoint.setDefaultOperationNamespace("http://localhost/employee");
+        return cxfEndpoint;
+    }
+
+    @Named
+    public CxfEndpoint employeeServiceEndpoint_getEmployeesByName() {
+        CxfEndpoint cxfEndpoint = new CxfEndpoint();
+        cxfEndpoint.setServiceClass(EmployeeServicePortType.class);
+        cxfEndpoint.setAddress(employeeSoapUrl);
+        cxfEndpoint.setWsdlURL("/wsdl/employee.wsdl");
+        cxfEndpoint.setDefaultOperationName("GetEmployeesByName");
+        cxfEndpoint.setDefaultOperationNamespace("http://localhost/employee");
+        return cxfEndpoint;
+    }
+}
